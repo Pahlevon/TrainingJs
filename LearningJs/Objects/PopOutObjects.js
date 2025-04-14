197ddefa21e66d503fe9b71ec13a89c690861c73
Pahlevon <rezaamaroof@gmail.com>
2025-04-14 17:59:46 +0330

    Third Js

---
+++ LearningJs/Objects/PopOutObjects.js
@@ -0,0 +1,23 @@
+const gameState = {
+    players: [],
+    gameName : "Our Beautifull Game",
+    copyrightYear : 2024,}
+
+
+const addPlayers = (players) => {
+    gameState.players.push(players)}
+
+const removeLatestAddedPlayer = () => { 
+    return gameState.players.pop()
+}
+
+addPlayers("Reza")
+addPlayers("Sara")
+addPlayers("Player with Bad Internet")
+
+console.log("Players After Adding",gameState.players) //List Of Players
+
+const valueofremoval = removeLatestAddedPlayer()
+console.log("The Player That Removed was :",valueofremoval)
+// console.log(returnLatestAddedPlayer)
+console.log("Current Players in The Game : ",gameState.players)
