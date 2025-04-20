ca0c56008c79e92c54ae6cf797b07ff955ea3ca3
Pahlevon <rezaamaroof@gmail.com>
2025-04-20 15:03:50 +0330

    Questions Added

--- LearningJs/Objects/PopOutObjects.js
+++ LearningJs/Objects/PopOutObjects.js
@@ -18,6 +18,6 @@ addPlayers("Player with Bad Internet")
 console.log("Players After Adding",gameState.players) //List Of Players
 
 const valueofremoval = removeLatestAddedPlayer()
-console.log("The Player That Removed was :",valueofremoval)
+console.log("The Player That was Removed  :",valueofremoval)
 // console.log(returnLatestAddedPlayer)
 console.log("Current Players in The Game : ",gameState.players)
